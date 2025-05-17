28c367543679c9e2eecaf5c9daed720c7311e74b
fardinabir <fardinabir@gmail.com>
2025-05-18 08:14:00 +0900

    fix: backend server 2 port typo

--- backend-services/service-2/service2.js
+++ backend-services/service-2/service2.js
@@ -1,4 +1,4 @@
 const http = require('http');
 http.createServer((req, res) => {
   res.end('Response from Server 2');
-}).listen(3001, () => console.log('Server 2 running on port 3002'));
+}).listen(3002, () => console.log('Server 2 running on port 3002'));
